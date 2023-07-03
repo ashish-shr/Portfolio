6472562f31f8363c972befcfa472e763d38ff0fb
Ashish Shrestha <ash.shrestha0360@gmail.com>
2023-07-03 19:46:14 +0545

    added shuttle animations⚡

--- assets/javascript/scroll.js
+++ assets/javascript/scroll.js
@@ -2,3 +2,28 @@
 function scrollToTop() {
   window.scrollTo({ top: 0, behavior: 'smooth' });
 }
+
+// Function to handle the intersection of elements
+function handleIntersection(entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("animate");
+      observer.unobserve(entry.target);
+    }
+  });
+}
+
+// Create an Intersection Observer instance
+const observer = new IntersectionObserver(handleIntersection, {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.2, // Adjust the threshold as needed
+});
+
+// Get all elements with the class 'card-animate'
+const cardAnimateElements = document.querySelectorAll(".card-animate");
+
+// Observe each 'card-animate' element
+cardAnimateElements.forEach((element) => {
+  observer.observe(element);
+});
